f733fd068d978894ac1f8a69854c69837a74f681
Parag Sharma <sharmaparag2004@gmail.com>
2025-08-25 18:14:47 +0530

    profile updated for both sides

--- creator-brand/src/component/org/sideBar.jsx
+++ creator-brand/src/component/org/sideBar.jsx
@@ -187,7 +187,7 @@ const Sidebar = ({
                 <button
                   className="flex items-center w-full px-3 py-2 text-sm text-white hover:bg-gray-900 hover:border-gray-600 rounded-lg border border-transparent transition-colors"
                   onClick={() => {
-                    navigate('/influencer_dashboard/portfolio'); // Use navigate for portfolio
+                    navigate('/org_dashboard/portfolio'); // Use navigate for portfolio
                     setActiveTab('portfolio');
                     setIsSidebarOpen(false);
                     setIsDropdownOpen(false);
